2fef8b83fe71d9c665b3b9657c8ae5a4a8490b12
Mathew Kuthur James <mathewkj2048@gmail.com>
2025-07-22 13:12:42 -0400

    added canvas

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,17 @@
-
 console.log("script active")
+
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+function resizeCanvas() {
+	const container = canvas.parentElement;
+	canvas.width = container.clientWidth;
+	canvas.height = container.clientHeight;
+	
+	ctx.fillStyle = '#8080ff'; // light blue to see where it is
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+resizeCanvas();
+window.addEventListener('resize', resizeCanvas);
